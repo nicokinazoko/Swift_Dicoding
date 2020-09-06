87e78fdb9fe2dd082cf7e5e809286051286ae3d7
nicokinazoko <38060004+nicokinazoko@users.noreply.github.com>
2020-09-06 18:57:53 +0700

    Create numeric-literals.swift

---
+++ type-safety-and-type-inference/numeric-literals.swift
@@ -0,0 +1,19 @@
+//numeric literal merupakan angka yang didapat dari berbagai bentuk
+//misal dari desimal, biner, dsb
+
+//Angka desimal, tanpa awalan.
+//Angka biner, dengan awalan 0b.
+//Angka oktal, dengan awalan 0o.
+//Angka heksadesimal, dengan awalan 0x.
+//contoh
+let decimalInteger      =   18
+let binaryInteger       =   0b10010
+let octalInteger        =   0o22
+let hexadecimalInteger  =   0x12
+
+//contoh angka dengan eksponen
+let decimalExponen      =   1.25e2
+let anotherDecimalEx    =   1.25e-2
+
+print(decimalInteger, binaryInteger, octalInteger, hexadecimalInteger, decimalExponen, anotherDecimalEx)
+//output : 18 18 18 18 125.0 0.0125
